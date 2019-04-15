eb75a151e8dcda2b8db04a80adb44e86823b9af6
obviousrebel <jalexander@stsci.edu>
2019-04-15 12:04:25 -0400

    progress

--- pivizion/pivizion.py
+++ pivizion/pivizion.py
@@ -1,27 +1,59 @@
 import os
 import io
 
-from google.cloud import vision
+from google.cloud import vision, texttospeech
 from google.cloud.vision import types
 
-client = vision.ImageAnnotatorClient()
+global is_pi
 
-file_name = "cat.jpeg"
+print(os.uname().sysname)
+if os.uname().sysname == 'raspberrypi':
+    import picamera
+    is_pi = True
+else:
+    from cv2 import *
+    is_pi = False
 
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
 
-image = types.Image(content=content)
+class PiVizion(object):
+    def get_image(self):
+        if is_pi:
+            print("Pi cam")
+        else:
+            cam = VideoCapture(0)
+            s, img = cam.read()
 
-response = client.label_detection(image=image)
-labels = response.label_annotations
+            if s:
+                # namedWindow("cam-test")
+                # imshow("cam-test", img)
+                # waitKey(0)
+                # destroyWindow("cam-test")
+                imwrite("filename.jpg", img)
 
-# response = client.text_detection(image=image)
-# texts = response.text_annotations
 
-print('Labels: ')
-for label in labels:
-    print(label.description)
+    def detect_labels(self):
+        client = vision.ImageAnnotatorClient()
 
-# for text in texts:
-#     print('\n"{}"'.format(text.description))
+        file_name = "filename.jpg"
+
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+        image = types.Image(content=content)
+
+        labels_response = client.label_detection(image=image)
+        labels = labels_response.label_annotations
+
+        text_response = client.text_detection(image=image)
+        texts = text_response.text_annotations
+
+        print('Labels: ')
+        print(labels[0].description)
+
+        print('Text: ')
+        print(texts[0].description)
+
+
+test = PiVizion()
+#test.get_image()
+test.detect_labels()
